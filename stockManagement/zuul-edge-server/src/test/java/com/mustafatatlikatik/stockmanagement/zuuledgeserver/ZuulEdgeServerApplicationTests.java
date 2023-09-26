dbb1cd91c2517705bed7ff78dc074d6c1a073f20
Mustafa Tatlıkatık <139918474+mustafatatlikatik@users.noreply.github.com>
2023-09-26 20:51:18 +0300

    spring stock management project with microservices

---
+++ stockManagement/zuul-edge-server/src/test/java/com/mustafatatlikatik/stockmanagement/zuuledgeserver/ZuulEdgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafatatlikatik.stockmanagement.zuuledgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulEdgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
